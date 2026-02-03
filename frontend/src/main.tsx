8213bbd3a1c6257b0c94f09b84bf2c3537085fc1
Deb001 <debudas@gmail.com>
2026-02-03 21:19:45 +0530

    Add frontend/src/main.tsx

---
+++ frontend/src/main.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import './index.css';
+import { AuthProvider } from './hooks/useAuth';
+import { ToastProvider } from './components/Toast';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <ToastProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ToastProvider>
+    </AuthProvider>
+  </React.StrictMode>
+);
